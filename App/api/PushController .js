92042731ab8dc1a8ae4aa343cf53fc5304aa580d
Vadim <bvntaev@gmail.com>
2021-06-15 22:18:51 +0300

    fix: solve the problem with notification sound

--- App/api/PushController .js
+++ App/api/PushController .js
@@ -1,15 +1,20 @@
 import React, {useEffect} from 'react';
 import {View, Button} from 'react-native';
 import PushNotification from 'react-native-push-notification';
+import {useNavigation} from '@react-navigation/core';
 
 export const PushController = () => {
-  const testPush = () => {
+  const navigation = useNavigation();
+
+  const testPush = async () => {
     PushNotification.localNotification({
-      channelId: 'fcm_fallback_notification_channel', // (required) channelId, if the channel doesn't exist, notification will not trigger.
+      channelId: 'fcm_fallback_notification_channel',
       title: 'My Notification Title', // (optional)
       message: 'My Notification Message', // (required)
-      playSound: true, // (optional) default: true
-      soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+      playSound: true,
+      soundName: 'default',
+
+      userInteraction: true,
     });
   };
 
@@ -22,10 +27,9 @@ export const PushController = () => {
       // (required) Called when a remote or local notification is opened or received
       onNotification: function (notification) {
         console.log('NOTIFICATION:', notification);
-        //   PushNotification.invokeApp(notification);
-        // process the notification here
-        //   testPush(notification);
+        navigation.navigate('Forum');
       },
+
       // Android only
       senderID: '1090501687137',
 
